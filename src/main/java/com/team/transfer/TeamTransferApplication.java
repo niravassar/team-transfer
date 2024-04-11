0b99e531fbcc783bc61db841882621af3fda4de7
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2024-04-11 09:37:18 -0500

    first

---
+++ src/main/java/com/team/transfer/TeamTransferApplication.java
@@ -0,0 +1,13 @@
+package com.team.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTransferApplication.class, args);
+	}
+
+}
