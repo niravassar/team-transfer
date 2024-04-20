eeb2829133b048d2aec1c5b6634c31f8bdfb5c3e
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2024-04-20 10:05:16 -0500

    getallteams

--- src/main/java/com/team/transfer/controller/TeamController.java
+++ src/main/java/com/team/transfer/controller/TeamController.java
@@ -1,18 +1,25 @@
 package com.team.transfer.controller;
 
 import com.team.transfer.contract.TransferFormContract;
-import com.team.transfer.exception.ValidationException;
+import com.team.transfer.domain.Team;
+import com.team.transfer.service.TeamService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@AllArgsConstructor
 public class TeamController {
 
+    private final TeamService teamService;
+
     @GetMapping("/team")
-    public String getAllTeams() {
-        throw new ValidationException("nirav threw it");
+    public List<Team> getAllTeams() {
+        return teamService.getAllTeams();
     }
 
     @PostMapping("/team/transfer")
--- src/test/java/com/team/transfer/controller/TeamControllerTest.java
+++ src/test/java/com/team/transfer/controller/TeamControllerTest.java
@@ -1,5 +1,7 @@
 package com.team.transfer.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.transfer.domain.Team;
 import com.team.transfer.service.TeamService;
 import org.junit.jupiter.api.Test;
@@ -7,10 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.team.transfer.utils.DataGenerator.getTeam;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TeamController.class)
 class TeamControllerTest {
@@ -18,12 +25,24 @@ class TeamControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
-    TeamService service;
+    TeamService teamService;
 
     @Test
-    void getAllTeams() {
-        List<Team> team = List.of();
+    void getAllTeams() throws Exception {
+        Team lvp = getTeam(13);
+        Team manU = getTeam(13);
+        List<Team> expected = List.of(lvp, manU);
+        when(this.teamService.getAllTeams()).thenReturn(expected);
+
+        MvcResult mvcResult =  mockMvc.perform(get("/team")).andExpect(status().isOk()).andReturn();
+        List<Team> actual = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<Team>>() {});
+
+        verify(this.teamService, times(1)).getAllTeams();
+        assertThat(actual).isEqualTo(expected);
     }
 
 }
