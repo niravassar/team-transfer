f5ed802b902d722ad538c4dc74855f8b12ba705e
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2024-04-21 21:54:13 -0500

    csrf disable

--- src/main/java/com/team/transfer/config/SecurityConfig.java
+++ src/main/java/com/team/transfer/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.team.transfer.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests( auth -> auth.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults()).build();
     }
 
